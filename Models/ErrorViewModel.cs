2e6852f81f87d31a538f4b17a4072d0213b63db8
Julio3021 <julio_antaurco@usmp.pe>
2024-08-17 17:37:56 -0500

    primer commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Tarea2_JulioAntaurco.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
